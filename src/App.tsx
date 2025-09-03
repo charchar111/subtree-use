1d98a03e59a8df8b6257a97878bc9ca146dd7a56
차태영 <3hjab@naver.com>
2025-09-03 21:35:35 +0900

    feat: :wrench: shardCN 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button as SCNButton } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,6 +29,8 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
+
+      <SCNButton className='cursor-pointer'>click me</SCNButton>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      // shardcn 설정과 연결되어 있으니, 함께 변경할 것
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
